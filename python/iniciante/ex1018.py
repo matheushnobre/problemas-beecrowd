2865cdb39729ed57e0d3ddbcd6a0b1bfb014a8d6
Matheus Henrique <matheus1204h@gmail.com>
2024-03-11 17:12:21 -0300

    adicionando problemas sql

--- python/iniciante/ex1018.py
+++
@@ -1,8 +0,0 @@
-# Runtime error
-cedulas = [100, 50, 20, 10, 5, 2, 1]
-n = int(input())
-print(n)
-for cedula in cedulas:
-    print(f"{n//cedula} nota(s) de R${cedula},00", end='\n')
-    n = n % cedula 
-
